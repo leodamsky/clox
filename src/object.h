36fe564c22941ffb2ea07db6bbee84c0468634c6
Vadym Leodamskyi <vadim.leodamsky@gmail.com>
2022-01-15 09:55:23 +0200

    Chapter 25: Closures

--- src/object.h
+++ src/object.h
@@ -7,19 +7,23 @@
 
 #define OBJ_TYPE(value) (AS_OBJ(value)->type)
 
+#define IS_CLOSURE(value) isObjType(value, OBJ_CLOSURE)
 #define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
 #define IS_NATIVE(value) isObjType(value, OBJ_NATIVE)
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
 
+#define AS_CLOSURE(value) ((ObjClosure *) AS_OBJ(value))
 #define AS_FUNCTION(value) ((ObjFunction *) AS_OBJ(value))
 #define AS_NATIVE(value) (((ObjNative *) AS_OBJ(value))->function)
 #define AS_STRING(value) ((ObjString *) AS_OBJ(value))
 #define AS_CSTRING(value) (((ObjString*) AS_OBJ(value))->chars)
 
 typedef enum {
+    OBJ_CLOSURE,
     OBJ_FUNCTION,
     OBJ_NATIVE,
     OBJ_STRING,
+    OBJ_UPVALUE,
 } ObjType;
 
 struct Obj {
@@ -30,6 +34,7 @@ struct Obj {
 typedef struct {
     Obj obj;
     int arity;
+    int upvalueCount;
     Chunk chunk;
     ObjString *name;
 } ObjFunction;
@@ -48,6 +53,28 @@ struct ObjString {
     uint32_t hash;
 };
 
+typedef struct ObjUpvalue {
+    Obj obj;
+    Value *location;
+    Value closed;
+    struct ObjUpvalue *next;
+} ObjUpvalue;
+
+typedef struct {
+    Obj obj;
+    ObjFunction *function;
+    ObjUpvalue **upvalues;
+    // GC:
+    // Storing the upvalue count in the closure is redundant because the ObjFunction
+    // that the ObjClosure references also keeps that count.
+    // As usual, this weird code is to appease the GC.
+    // The collector may need to know an ObjClosure’s upvalue array size
+    // after the closure’s corresponding ObjFunction has already been freed.
+    int upvalueCount;
+} ObjClosure;
+
+ObjClosure *newClosure(ObjFunction *function);
+
 ObjFunction *newFunction();
 
 ObjNative *newNative(NativeFn function);
@@ -55,6 +82,8 @@ ObjNative *newNative(NativeFn function);
 struct ObjString *takeString(char *chars, int length);
 
 struct ObjString *copyString(const char *chars, int length);
+
+ObjUpvalue *newUpvalue(Value *slot);
 
 void printObject(Value value);
 
